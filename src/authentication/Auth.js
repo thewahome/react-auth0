18fe723886e014c3821ea5cbf0da09505aa49889
Charles Wahome <thewahome.cw@gmail.com>
2020-09-04 19:43:36 +0300

    check if user is authenticated

--- src/authentication/Auth.js
+++ src/authentication/Auth.js
@@ -40,4 +40,9 @@ export default class Auth {
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
   }
+
+  isAuthenticated () {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home (props) {
+  const { isAuthenticated, login } = props.auth;
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={props.auth.login}>Log in</button>
+      {isAuthenticated() ? <Link to='/profile'>View Profile</Link> : (
+        <button onClick={login}>Log in</button>
+      )}
     </div>
   )
 }
