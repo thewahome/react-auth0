c72f7b488141c3e66156533154079511acf5d9b2
Charles Wahome <thewahome.cw@gmail.com>
2020-09-05 02:09:14 +0300

    configure express to parse jwt tokens

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 require('dotenv').config();
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}`,
+  algorithms: ['RS256']
+})
 
 const api = express();
+api.use(checkJwt);
+
 api.get('/public', function (req, res) {
   res.json({
     message: 'Hello from a public api'
+  });
+});
+
+api.get('/private', checkJwt, function (req, res) {
+  res.json({
+    message: 'Hello from a private api'
   });
 });
 
